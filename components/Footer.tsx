b14a48dcf637b8b07801cf2c18e956582db7a30c
Rupam Shil <rupamshil111@gmail.com>
2025-06-23 13:41:12 +0530

    feat: implement footer component with social links and navigation

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,76 @@
+import {
+	DiscordLogoIcon,
+	GithubLogoIcon,
+	XLogoIcon,
+	YoutubeLogoIcon,
+} from '@phosphor-icons/react';
+import Link from 'next/link';
 import React from 'react';
 
+const LeftLinks = [
+	{
+		name: 'Developers',
+		href: 'https://docs.hashstack.finance/developers',
+	},
+	{
+		name: 'Documentation',
+		href: 'https://docs.hashstack.finance/hub',
+	},
+	{
+		name: 'Community',
+		href: 'https://hashstack.community',
+	},
+];
+
+const RightLinks = [
+	{
+		icon: DiscordLogoIcon,
+		href: 'https://hashstack.community',
+	},
+	{
+		icon: XLogoIcon,
+		href: 'https://x.com/0xhashstack',
+	},
+	{
+		icon: GithubLogoIcon,
+		href: 'https://github.com/0xHashstack/',
+	},
+	{
+		icon: YoutubeLogoIcon,
+		href: 'https://www.youtube.com/@hashstack',
+	},
+];
 function Footer() {
-	return <div>saf</div>;
+	return (
+		<div className="flex items-center  justify-between py-6">
+			<div className="flex items-center gap-6">
+				{LeftLinks.map((link) => (
+					<Link
+						key={link.name}
+						href={link.href}
+						target="_blank"
+						className="text-sm text-neutral-400"
+					>
+						{link.name}
+					</Link>
+				))}
+			</div>
+			<div className="flex items-center gap-4">
+				{RightLinks.map((link, index) => (
+					<Link
+						key={index}
+						href={link.href}
+						target="_blank"
+						className="text-sm text-black"
+					>
+						<div className="w-7 h-7 rounded-full bg-neutral-200 flex items-center justify-center">
+							<link.icon size={16} />
+						</div>
+					</Link>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default Footer;
